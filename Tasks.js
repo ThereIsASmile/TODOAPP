98300761d03c717bd9ed037019b1a9d6dcec0b1a
ThereIsASmile <57597467+ThereIsASmile@users.noreply.github.com>
2019-11-16 14:40:49 -0500

    Add files via upload

---
+++ Tasks.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class Tasks extends Component {
+  toggleTask = event => {
+    var task = event.target.parentElement;
+    var buttonComplete = task.querySelector("#completed");
+    var buttonUndo = task.querySelector("#undo");
+
+    if (task.classList.contains("complete")) {
+      task.classList.remove("complete");
+      buttonComplete.classList.remove("hidden");
+      buttonUndo.classList.add("hidden");
+    } else {
+      task.classList.add("complete");
+      buttonComplete.classList.add("hidden");
+      buttonUndo.classList.remove("hidden");
+    }
+  };
+
+  render() {
+    return (
+      <ul>
+        {this.props.items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button id="completed" onClick={this.toggleTask}>
+              Completed
+            </button>
+            <button id="undo" className="hidden" onClick={this.toggleTask}>
+              Undo
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+import Tasks from "./Tasks";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+  onChange = event => {
+    this.setState({ term: event.target.value }, function() {
+      console.log(this.state.term);
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState(
+      {
+        term: "",
+        items: [...this.state.items, this.state.term]
+      },
+      function() {
+        console.log(this.state.items);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className="todoApp">
+        <form className="App" onSubmit={this.onSubmit}>
+          <input
+            placeholder="What do you need to do?"
+            onChange={this.onChange}
+            value={this.state.term}
+          />
+          <button type="submit">Add task</button>
+        </form>
+        <Tasks items={this.state.items} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
